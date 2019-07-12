17bb089645666e78529717d275334edc8a563a91
Josh <josh@zype.co.nz>
2019-07-12 12:40:48 +1200

    Student list and individual report added, initial work on the project visual

--- app-sqla.py
+++ app-sqla.py
@@ -10,10 +10,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Creates a Flask object called 'app' that we can use throughout the programme
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/josh/Projects/StudentApplication/db/report.db'
+db = SQLAlchemy(app)
 
 # DB Connection
 def query_db(query, single=False):
-    db = sqlite3.connect('db/report.db')
+    
     cursor = db.cursor()
     cursor.execute(query)
     result = cursor.fetchone() if single else cursor.fetchall()
@@ -22,14 +24,13 @@ def query_db(query, single=False):
 # This is the function that controls the main page of the web site
 @app.route("/")
 def index():
-  query = query_db("SELECT Students.StudentID, Students.StudentName, StandardID, AssessedLevel from Assessment join Students using (StudentID)")
-  students = [dict(studentID=row[0], studentName=row[1], standardID=row[2], assessedLevel=row[3]) for row in query]
+  query = query_db("SELECT Students.StudentName, StandardID, AssessedLevel from Assessment join Students using (StudentID)")
+  assessment = [dict(student=row[0], standard=row[1], assessed=row[2]) for row in query]
 
-  return render_template('students.html',
+  return render_template('assessment.html',
                           title="Progress Report",
-                          students=students)
+                          assessment=assessment)
 
-'''
 # This is the function shows the Student page
 @app.route("/students")
 def students():
@@ -39,24 +40,6 @@ def students():
   return render_template('students.html',
                           title="Students",
 													students=students)
-'''
-
-# Function that displays the individual report for a student
-@app.route("/students/<int:id>")
-def student_edit(id): # Function definition contains a parameter for ID
-  single="TRUE"
-## Database Query where ID is selected from a link in the GUI
-  query = query_db("SELECT Students.StudentID, Students.StudentName, StandardID, AssessedLevel from Assessment join Students using (StudentID) WHERE Students.StudentID={0} ".format(id), single)
-
-## Page Not Found 
-  if not query:
-    abort(404)
-
-  return render_template('report.html',
-                          title="Student Report",
-                          student=query,
-                          id=id)
-
 
 
 # This function deals with any missing pages and shows the Error page
